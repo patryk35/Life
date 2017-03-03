6dfd65e58565083ce0d714a381807ea3d8f452fa
patryk35 <pat35@op.pl>
2017-03-03 17:23:34 +0100

    v1.2.1

--- gameOperator.c
+++ gameOperator.c
@@ -7,6 +7,7 @@
 #include "boardGanerator.h"
 #include "fileRead.h"
 #include <stdlib.h>
+#include <stdio.h>
 /*
  * Error List
  * -1 - [gameOperator] SettingsError -
@@ -43,7 +44,11 @@ int startGame(gameBoard_t * gameBoard, int isBoardLoaded){
 
 int gameSimulation(gameBoard_t * gameBoard, int countOfAdjacentCells, int generationsCount){
     int boardSize = gameBoard->boardSize;
-    printFile("", gameBoard);
+    printFile("", gameBoard); /*
+ *
+ *
+ *
+ */
     for (int l =0;l < generationsCount; l++){
         int ** newFields;
         newFields= malloc(sizeof(newFields)* boardSize);
@@ -59,19 +64,23 @@ int gameSimulation(gameBoard_t * gameBoard, int countOfAdjacentCells, int genera
         for(int i =1;i<boardSize-1;i++){ // from 1 cause 0 is part of border, and gameBoard.height -1 too
             for(int j =1;j<boardSize-1;j++) {
                 if(countOfAdjacentCells==8)
+                {
                     newFields[i][j]=checkCellMooreNeighborhood(gameBoard->fields, i, j);
+                    printf("%d  \n", gameBoard->fields[i][j]);
+                }
                 else
                     newFields[i][j]=checkCellVonNeumannNeighborhood(gameBoard->fields, i, j);
             }
         }
         gameBoard->fields=newFields; // usunac mallocowanie z tworzenia tego elementu
         //generowanie obrazów
+        printFile("", gameBoard);
 
     }
 }
 
 int checkCellMooreNeighborhood(int ** fields, int x, int y){
-    int lifeStatus = fields[x][y];
+    int isAlive = fields[x][y];
     int aliveNeighbours = 0;
     for(int i =-1; i <= 1; i++){
         for(int j =-1; j <= 1; j++){
@@ -81,15 +90,18 @@ int checkCellMooreNeighborhood(int ** fields, int x, int y){
         }
     }
 
-    if(lifeStatus == 1){
+    if(isAlive == 1){
         if(aliveNeighbours == 2 || aliveNeighbours == 3)
         {
-            return 0;
-        } else if(aliveNeighbours == 3){
+            printf("1. ");
             return 1;
+        } else{
+            printf("2. ");
+            return 0;
         }
-    }else{
-        return lifeStatus;
+    }else if(ifAvlfe){
+        printf("3. ");
+        return isAlive;
     }
 
 
@@ -97,10 +109,10 @@ int checkCellMooreNeighborhood(int ** fields, int x, int y){
 
 }
 int checkCellVonNeumannNeighborhood(int ** fields, int x, int y){
-    int lifeStatus = fields[x][y];
+    int isAlive = fields[x][y];
     int aliveNeighbours = fields[x+1][y] + fields[x-1][y] + fields[x][y+1] + fields[x][y+1];
 
-    if(lifeStatus == 1){
+    if(isAlive == 1){
         if(aliveNeighbours == 2 || aliveNeighbours == 3)
         {
             return 0;
@@ -108,6 +120,6 @@ int checkCellVonNeumannNeighborhood(int ** fields, int x, int y){
             return 1;
         }
     }else{
-        return lifeStatus;
+        return isAlive;
     }
 }

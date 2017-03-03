2dada4b905f77fa83ffbc3338d724686874bd731
zarczukp <pawel_zarczuk@o2.pl>
2017-03-03 11:14:54 -0500

    Sprawdzenie

--- gameSettings.c
+++ gameSettings.c
@@ -7,7 +7,7 @@
 int loadGameSettings(char * fileName, gameSettings_t * settings)
 {
     FILE * file = fopen(fileName,"r");
-    settings->countOfAdjacentCells= 4; // liczba sąsiednich komórek opcej: 4 lub 8
+    settings->countOfAdjacentCells= 8; // liczba sąsiednich komórek opcej: 4 lub 8
     settings->isBoardLoaded=0; // jeżeli 1 to mamy wczytać plik z planszą, jeżeli 0  to losujemy jakąś plansze o wymiarach z settings->deflautBoardSize;
     settings->deflautBoardSize=10;
     settings->edgeSettings=1;

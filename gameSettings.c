a3f36dd4bd452115c255f3a7c296150268be45bc
zarczukp <pawel_zarczuk@o2.pl>
2017-03-09 13:57:53 -0500

    Unix version

--- gameSettings.c
+++ gameSettings.c
@@ -22,28 +22,28 @@ int loadGameSettings(char * fileName, gameSettings_t * settings)
             settings->neighborhoodSettings=ValueTmp;
             counterofcalling++;
         }
-        if(strcmp(tmpFirstString,"defaultBoardSize")==0){
+        else if(strcmp(tmpFirstString,"defaultBoardSize")==0){
             int ValueTmp = atoi(tmpSecondString);
             if(ValueTmp<=0)
                 return -3; //"Error: Failure in configuration file. Value \"defaultBoardSize\" must be positive value.
             settings->defaultBoardSize=ValueTmp+2; // +2, cause cells are stored from index 1 to index equal boardSize-1, other space is designed for border
             counterofcalling++;
         }
-        if(strcmp(tmpFirstString,"edgeSettings")==0){
+        else if(strcmp(tmpFirstString,"edgeSettings")==0){
             int ValueTmp = atoi(tmpSecondString);
             if(ValueTmp!=0 && ValueTmp!=1 && ValueTmp!=2)
                 return -4;//Error: Failure in configuration file. Value \"edgeSettings\" must be 0 or 1 or 2.
             settings->edgeSettings = ValueTmp;
             counterofcalling++;
         }
-        if(strcmp(tmpFirstString,"defaultGenerationCount")==0){
+        else if(strcmp(tmpFirstString,"defaultGenerationCount")==0){
             int ValueTmp = atoi(tmpSecondString);
             if(ValueTmp<1 || ValueTmp>999)
                 return -5; // Error: Failure in configuration file. Value "defaultGenerationCount" must be positive and less than 1000
             settings->defaultGenerationCount = ValueTmp;
             counterofcalling++;
         }
-        if(strcmp(tmpFirstString,"cellSize")==0){
+        else if(strcmp(tmpFirstString,"cellSize")==0){
             int ValueTmp = atoi(tmpSecondString);
             if(ValueTmp<0)
                 return 2; //Error: Failure in configuration file. Value "cellSize" must be positive
